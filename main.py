74c50aebb9bc2b2a5d17a72a45e2a7a5363d4a52
aswathinrp <90610747+aswathinrp@users.noreply.github.com>
2022-12-21 11:28:40 +0530

    first commit

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import cv2
-img=cv2.imread('pets.jpg')
+img=cv2.imread('pets-2.jpg')
 
-classNames=[]
+classNames=[]  #importing the coco file
 classFile = 'coco.names'
-with open(classFile,'rt') as f:
-    classNames=f.read().rstrip('\n').split('\n')
-configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-weightsPath = 'frozen_inference_graph.pb'
+with open(classFile,'rt') as f: #open our file and we need to read it
+    classNames=f.read().rstrip('\n').split('\n') #strip it and split it based on newline 
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'  # import the configuration file
+weightsPath = 'frozen_inference_graph.pb'   # import the weight file
 
-net = cv2.dnn_DetectionModel(weightsPath,configPath)
+net = cv2.dnn_DetectionModel(weightsPath,configPath) # create our model, and
 net.setInputSize(320,320)
 net.setInputScale(1.0/ 127.5)
 net.setInputMean((127.5, 127.5 ,127.5))
 net.setInputSwapRB(True)
 
 classIds, confs, bbox = net.detect(img,confThreshold=0.5)
-print(classIds,bbox)
+print(classIds,bbox) # it will give the classids and bbox values
 
-for classId, confidence, box in zip(classIds.flatten(),confs.flatten(),bbox):
+for classId, confidence, box in zip(classIds.flatten(),confs.flatten(),bbox): # 3 informations - classids,confidence and bbox
     cv2.rectangle(img,box,color=(0,255,0),thickness=3)
     cv2.putText(img,classNames[classId-1],(box[0]+10,box[1]+30),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
 cv2.imshow('img',img)
--- video.py
+++ video.py
@@ -1,11 +1,9 @@
 import cv2
  
 cap=cv2.VideoCapture(0)
-cap.set(3,640)
+cap.set(3,640)  #
 cap.set(4,480)
 
-
- 
 classNames=[]
 classFile = 'coco.names'
 with open(classFile,'rt') as f:
